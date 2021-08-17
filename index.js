ea08b697ea7e4406e0f5c64706628165a3ea99db
Aagd12 <80593478+Aagd12@users.noreply.github.com>
2021-08-18 00:41:25 +0530

    setup css

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const path = require("path");
-require("./src/db/conn");
+let s = require("./src/db/conn");
 const hbs = require("hbs");
 const app = express();
-
+const mongoose = require('mongoose');
 const port = process.env.PORT || 3000
     // setting the path
 const static_path = path.join(__dirname, "./public");
@@ -14,6 +14,18 @@ const partials_path = path.join(__dirname, "./templates/partials");
 app.use('/css', express.static(path.join(__dirname, "./node_modules/bootstrap/dist/css")))
 app.use('/js', express.static(path.join(__dirname, "./node_modules/bootstrap/dist/js")))
 app.use('/jq', express.static(path.join(__dirname, "./node_modules/bootstrap/dist/jq")))
+const kittySchema = new mongoose.Schema({
+    fname: String,
+    name: String,
+    age: Number,
+    gender: String,
+    email: String,
+    phone: Number,
+    password: Number
+});
+
+
+const contactus = mongoose.model('contactus', kittySchema);
 
 app.use(express.static(static_path))
 app.set("view engine", "hbs");
@@ -27,6 +39,15 @@ app.get("/contact", (req, res) => {
 });
 app.get("/about", (req, res) => {
     res.render("about.hbs");
+});
+app.post('/contact', (req, res) => {
+    var myData = new contactus(req.body);
+    myData.save().then(() => {
+            res.render('index.hbs');
+        }).catch(() => {
+            res.status(400).send('item was not saved to the databse')
+        })
+        // res.status(200).render('contact.pug');
 });
 app.listen(port, () => {
     console.log(`server is running on port ${port} `);
---
+++ public/index2.js
@@ -0,0 +1,10 @@
+let read3 = document.getElementById("readmore");
+let read4 = document.getElementById("readmore");
+read3.addclassList = read;
+document.querySelectorAll("read1").onclick(() => {
+    read3.addclassList = read;
+    read3.style.display = block;
+})
+document.querySelectorAll("read2").addEventListener("mousemove"), (() => {
+    read4.style.display = block;
+})
